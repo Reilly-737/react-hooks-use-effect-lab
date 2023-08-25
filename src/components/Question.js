2045f512069c382b2ad6df5fef3dbf9e70351b26
+ <+>
2023-08-25 13:56:51 -0600

    finshed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -11,6 +11,18 @@ function Question({ question, onAnswered }) {
   }
 
   const { id, prompt, answers, correctIndex } = question;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    } , 1000);
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]);
 
   return (
     <>
